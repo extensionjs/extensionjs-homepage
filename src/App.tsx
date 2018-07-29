9e9c10e0b1b0a36f925622eeca2f9c112ef9f1aa
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-07-29 14:01:49 +0200

    Replaced css/sass with jss

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,15 @@ import { Button } from 'evergreen-ui';
 import * as React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
     return (
       <React.Fragment>
         <CssBaseline />
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          <Button>I am using 🌲 Evergreen!</Button>
-        </div>
+          <div>
+            <Button>I am using 🌲 Evergreen!</Button>
+          </div>
       </div>
       </React.Fragment>
     );
